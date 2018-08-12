cb0f146ae5aafa703191db47dfb70ae1b682d0b7
Terence Tsang <terence@madhead.com>
2018-08-12 18:40:12 +0800

    Create brackets-extended-edition.cs

---
+++ brackets-extended-edition.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text.RegularExpressions;
+class S
+{
+    static void Main()
+    {
+        string regex = @"[\(\)]\w*[\(\)]|[{}]\w*[{}]|[\[\]]\w*[\[\]]|[<>]\w*[<>]";
+        for (int i = 0, N = int.Parse(Console.ReadLine()); i < N; i++)
+        {
+            string text = Console.ReadLine();
+            while(Regex.Match(text, regex).Success){
+                text = Regex.Replace(text, regex, "");
+            }
+            Console.WriteLine(Regex.Match(text, @"[\[\]\(\){}<>]").Success ? "false": "true");
+        }
+    }
+}
