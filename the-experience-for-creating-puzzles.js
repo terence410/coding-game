5c9cf8fe5dbeb92e85d8a34627eec5f56ef16f97
Terence Tsang <terence@madhead.com>
2018-08-26 07:34:54 +0800

    Update the-experience-for-creating-puzzles.js

--- the-experience-for-creating-puzzles.js
+++ the-experience-for-creating-puzzles.js
@@ -1,1 +1,1 @@
-print((b=(l,x)=>x>=0?b(++l,x-(l*Math.sqrt(l)*10|0)):`${l}\n${-x}`).call(null,+(r=readline)(),-r()+r()*300));
+print((b=(l,x)=>x>=0?b(++l,x-(l*Math.sqrt(l)*10|0)):`${l}\n${-x}`)(+(r=readline)(),-r()+r()*300));
