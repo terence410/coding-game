82c5671d6750e4079250c66fc4cb2316b06ed25e
Terence Tsang <terence@madhead.com>
2018-12-29 12:16:44 +0800

    Update expand-the-polynomial.js

--- expand-the-polynomial.js
+++ expand-the-polynomial.js
@@ -3,7 +3,7 @@ let found0 = poly.match(/\((.*)\)\^2/);
 let found1 = poly.match(/(\([^\)]*\)\^?[0-9]?)/g);
 if(found0) found1 = [found0[1], found0[1]];
 
-let answers = null;
+let answers = [1, ...Array(9).fill(0)];
 found1.forEach(equation => {
     let found2 = equation.match(/([+-]?[^\+\-\(\)]*)/g);
     let holder = Array(10).fill(0);
@@ -12,23 +12,18 @@ found1.forEach(equation => {
         holder[+power || (x === 'x' ? 1: 0)] = isNaN(+number) ? +(number + "1"): +number || 1;
     });
     
-    if(!answers){
-        answers = holder;
-        
-    }else{
-        let temp = Array(10).fill(0);
-        holder.forEach((v, i) =>  v ? answers.forEach((vv, ii) => vv ? temp[i + ii] += v * vv: 0): 0);
-        answers = temp;
-    }
+    let temp = Array(10).fill(0);
+    holder.forEach((v, i) =>  v && answers.forEach((vv, ii) => vv && (temp[i + ii] += v * vv)));
+    answers = temp;
 });
 
 let outputs = [];
 answers.reverse().forEach((vv, ii) => {
     if(vv != 0){
         let power = (10 - ii - 1);
-        let alphabet = power > 1 ? "x^" + power: (power > 0 ? "x": "");
+        let alphabet = power > 1 ? "x^" + power: (power ? "x": "");
         let sign = vv > 0 ? (outputs.length ? "+": "") : "-";
-        let number = power === 0 || Math.abs(vv) != 1 ? Math.abs(vv): "";
+        let number = !power || Math.abs(vv) != 1 ? Math.abs(vv): "";
         outputs.push(`${sign}${number}${alphabet}`);
     }
 })
