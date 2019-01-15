3b376473ecf6e81028876979d489bd6cae5f4e13
Terence Tsang <terence@madhead.com>
2019-01-15 23:42:28 +0800

    Create these-romans-are-crazy.js

---
+++ these-romans-are-crazy.js
@@ -0,0 +1,26 @@
+let [rom1, rom2] = [readline(), readline()];
+let replace = param => [...numbers.entries()].reduce((a, b) => a.replace(new RegExp(b[1], 'g'), `+${b[0]}`), param);
+let numbers = new Map([
+    [900, "CM"], 
+    [400, "CD"], 
+    [90, "XC"], 
+    [40, "XL"], 
+    [9, "IX"],
+    [4, "IV"],
+    [1000, "M"], 
+    [500, "D"], 
+    [100, "C"], 
+    [50, "L"], 
+    [10, "X"], 
+    [5, "V"],
+    [1, "I"]
+]);
+let value = eval(replace(rom1) + replace(rom2));
+let output = "";
+for(let [int, str] of [...numbers.entries()].sort((a, b) => b[0] - a[0])){
+    while(value >= int){
+        output += str;
+        value -= int;
+    }
+}
+print(output);
